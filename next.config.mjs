16b2504d3e65f9cde0eeb0200954be6e84931ab4
Laurie Voss <github@seldo.com>
2024-08-25 12:32:52 -0700

    Missing tiktoken wasm

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
-/** @type {import('next').NextConfig} */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-      },    
+  experimental: {
+    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "node_modules/tiktoken/lite/tiktoken_bg.wasm",
+            to: "tiktoken_bg.wasm",
+            toType: "file"
+          },
+        ],
+      })
+    );
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
