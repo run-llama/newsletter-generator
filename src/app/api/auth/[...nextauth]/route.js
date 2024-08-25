b3749a07c74ca45d22fca35354316defea5ec71f
Laurie Voss <github@seldo.com>
2024-08-25 11:59:46 -0700

    Limit auth to one user

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,16 @@
 import NextAuth from "next-auth"
 import TwitterProvider from "next-auth/providers/twitter"
 
+async function getTwitterUsername(accessToken) {
+    const response = await fetch('https://api.twitter.com/2/users/me', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const data = await response.json();
+    return data.data.username;
+  }
+
 export const authOptions = {
     providers: [
       TwitterProvider({
@@ -10,6 +20,15 @@ export const authOptions = {
       })
     ],
     callbacks: {
+        async signIn({ user, account }) {
+            const username = await getTwitterUsername(account.access_token);
+            // Check if the user's Twitter username matches the allowed username
+            if (username === process.env.TWITTER_USER) {
+              return true; // Allow sign in
+            } else {
+              return false; // Deny sign in
+            }
+          },        
       async jwt({ token, account }) {
         // Persist the OAuth access_token to the token right after signin
         if (account) {
