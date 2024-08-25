5000572088ab34fd4abbe58aa040d532979fa1db
Laurie Voss <github@seldo.com>
2024-08-25 13:50:39 -0700

    Remove commented config

--- next.config.mjs
+++ next.config.mjs
@@ -5,22 +5,6 @@ const nextConfig = {
     serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
     outputFileTracingIncludes: { "/api/*": ["./node_modules/**/*.wasm"], }
   },
-  /*
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "node_modules/tiktoken/lite/tiktoken_bg.wasm",
-            to: "tiktoken_bg.wasm",
-            toType: "file"
-          },
-        ],
-      })
-    );
-    return config;
-  },
-  */
 };
 
 export default nextConfig;
